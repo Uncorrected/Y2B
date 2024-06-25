a6e09331a3f0abe2708ae7b2833f9d8ddc5cc0e9
Yang <67717529+Uncorrected@users.noreply.github.com>
2024-06-25 19:37:14 +0800

    Update upload.py

--- upload.py
+++ upload.py
@@ -10,7 +10,6 @@ import argparse
 import logging
 import sys
 
-
 UPLOAD_SLEEP_SECOND = 60 * 2  # 2min
 UPLOADED_VIDEO_FILE = "uploaded_video.json"
 CONFIG_FILE = "config.json"
@@ -20,77 +19,90 @@ PROXY = {
     "https": os.environ.get("https_proxy", None)
 }
 
-
 def get_gist(_gid, token):
     """通过 gist id 获取已上传数据"""
-    rsp = requests.get(
-        "https://api.github.com/gists/" + _gid,
-        headers={
-            "Accept": "application/vnd.github+json",
-            "Authorization": "Bearer " + token,
-        },
-        verify=VERIFY,
-    )
-    if rsp.status_code == 404:
-        raise Exception("gist id 错误")
-    if rsp.status_code == 403 or rsp.status_code == 401:
-        raise Exception("github TOKEN 错误")
-    _data = rsp.json()
-    uploaded_file = _data.get("files", {}).get(
-        UPLOADED_VIDEO_FILE, {}).get("content", "{}")
-    c = json.loads(_data["files"][CONFIG_FILE]["content"])
-    t = json.loads(_data["files"][COOKIE_FILE]["content"])
     try:
-        u = json.loads(uploaded_file)
-        return c, t, u
-    except Exception as e:
-        logging.error(f"gist 格式错误，重新初始化:{e}")
+        rsp = requests.get(
+            "https://api.github.com/gists/" + _gid,
+            headers={
+                "Accept": "application/vnd.github+json",
+                "Authorization": "Bearer " + token,
+            },
+            verify=VERIFY,
+        )
+        rsp.raise_for_status()  # 主动抛出异常，如果状态码不是 200
+        _data = rsp.json()
+        uploaded_file = _data.get("files", {}).get(
+            UPLOADED_VIDEO_FILE, {}).get("content", "{}")
+        c = json.loads(_data["files"][CONFIG_FILE]["content"])
+        t = json.loads(_data["files"][COOKIE_FILE]["content"])
+        try:
+            u = json.loads(uploaded_file)
+            return c, t, u
+        except Exception as e:
+            logging.error(f"gist 格式错误，重新初始化: {e}")
+    except requests.exceptions.RequestException as e:
+        if rsp.status_code == 404:
+            raise Exception("gist id 错误")
+        elif rsp.status_code == 403 or rsp.status_code == 401:
+            raise Exception("github TOKEN 错误")
+        else:
+            raise Exception(f"获取 gist 数据时出现未知错误: {e}")
     return c, t, {}
 
-
 def update_gist(_gid, token, file, data):
-    rsp = requests.post(
-        "https://api.github.com/gists/" + _gid,
-        json={
-            "description": "y2b暂存数据",
-            "files": {
-                file: {
-                    "content": json.dumps(data, indent="  ", ensure_ascii=False)
-                },
-            }
-        },
-        headers={
-            "Accept": "application/vnd.github+json",
-            "Authorization": "Bearer " + token,
-        },
-        verify=VERIFY,
-    )
-    if rsp.status_code == 404:
-        raise Exception("gist id 错误")
-    if rsp.status_code == 422:
-        raise Exception("github TOKEN 错误")
-
+    try:
+        rsp = requests.post(
+            "https://api.github.com/gists/" + _gid,
+            json={
+                "description": "y2b暂存数据",
+                "files": {
+                    file: {
+                        "content": json.dumps(data, indent="  ", ensure_ascii=False)
+                    },
+                }
+            },
+            headers={
+                "Accept": "application/vnd.github+json",
+                "Authorization": "Bearer " + token,
+            },
+            verify=VERIFY,
+        )
+        rsp.raise_for_status()  # 主动抛出异常，如果状态码不是 200
+    except requests.exceptions.RequestException as e:
+        if rsp.status_code == 404:
+            raise Exception("gist id 错误")
+        elif rsp.status_code == 422:
+            raise Exception("github TOKEN 错误")
+        else:
+            raise Exception(f"更新 gist 数据时出现未知错误: {e}")
 
 def get_file_size(filename):
-    sz = os.path.getsize(filename)
-    return int(sz/1024/1024)
-
+    try:
+        sz = os.path.getsize(filename)
+        return int(sz/1024/1024)
+    except OSError as e:
+        logging.error(f"获取文件大小出错: {e}")
+        return 0
 
 def get_video_list(channel_id: str):
-    res = requests.get(
-        "https://www.youtube.com/feeds/videos.xml?channel_id=" + channel_id).text
-    res = xmltodict.parse(res)
-    ret = []
-    for elem in res.get("feed", {}).get("entry", []):
-        ret.append({
-            "vid": elem.get("yt:videoId"),
-            "title": elem.get("title"),
-            "origin": "https://www.youtube.com/watch?v=" + elem["yt:videoId"],
-            "cover_url": elem["media:group"]["media:thumbnail"]["@url"],
-            # "desc": elem["media:group"]["media:description"],
-        })
-    return ret
-
+    try:
+        res = requests.get(
+            "https://www.youtube.com/feeds/videos.xml?channel_id=" + channel_id).text
+        res = xmltodict.parse(res)
+        ret = []
+        for elem in res.get("feed", {}).get("entry", []):
+            ret.append({
+                "vid": elem.get("yt:videoId"),
+                "title": elem.get("title"),
+                "origin": "https://www.youtube.com/watch?v=" + elem["yt:videoId"],
+                "cover_url": elem["media:group"]["media:thumbnail"]["@url"],
+                # "desc": elem["media:group"]["media:description"],
+            })
+        return ret
+    except requests.exceptions.RequestException as e:
+        logging.error(f"获取视频列表时出错: {e}")
+        return []
 
 def select_not_uploaded(video_list: list, _uploaded: dict):
     ret = []
@@ -102,7 +114,6 @@ def select_not_uploaded(video_list: list, _uploaded: dict):
         ret.append(i)
     return ret
 
-
 def get_all_video(_config):
     ret = []
     for i in _config:
@@ -113,7 +124,6 @@ def get_all_video(_config):
                 "config": i
             })
     return ret
-
 
 def download_video(url, out, format):
     try:
@@ -136,12 +146,13 @@ def download_video(url, out, format):
         logging.error("未知错误:" + out)
         raise e
 
-
 def download_cover(url, out):
-    res = requests.get(url, verify=VERIFY).content
-    with open(out, "wb") as tmp:
-        tmp.write(res)
-
+    try:
+        res = requests.get(url, verify=VERIFY).content
+        with open(out, "wb") as tmp:
+            tmp.write(res)
+    except requests.exceptions.RequestException as e:
+        logging.error(f"下载封面时出错: {e}")
 
 def upload_video(video_file, cover_file, _config, detail):
     title = detail['title']
@@ -179,7 +190,7 @@ def upload_video(video_file, cover_file, _config, detail):
         stdout=subprocess.PIPE,
     )
     p.wait()
-    if p.returncode != 0:
+    if p.returncode!= 0:
         raise Exception(p.stdout.read())
     buf = p.stdout.read().splitlines(keepends=False)
     if len(buf) < 2:
@@ -197,7 +208,6 @@ def upload_video(video_file, cover_file, _config, detail):
         raise e
     logging.debug(f'上传完成，返回：{data}')
     return data
-
 
 def process_one(detail, config):
     logging.info(f'开始：{detail["vid"]}')
@@ -218,30 +228,31 @@ def process_one(detail, config):
     os.remove(detail["vid"] + ".jpg")
     return ret
 
-
 def upload_process(gist_id, token):
-    config, cookie, uploaded = get_gist(gist_id, token)
-    with open("cookies.json", "w", encoding="utf8") as tmp:
-        tmp.write(json.dumps(cookie))
-    need_to_process = get_all_video(config)
-    need = select_not_uploaded(need_to_process, uploaded)
-    for i in need:
-        ret = process_one(i["detail"], i["config"])
-        if ret is None:
-            continue
-        i["ret"] = ret
-        uploaded[i["detail"]["vid"]] = i
-        update_gist(gist_id, token, UPLOADED_VIDEO_FILE, uploaded)
-        logging.info(
-            f'上传完成,vid:{i["detail"]["vid"]},aid:{ret["data"]["aid"]},bvid:{ret["data"]["bvid"]}')
-        logging.debug(f"防验证码，暂停 {UPLOAD_SLEEP_SECOND} 秒")
-        time.sleep(UPLOAD_SLEEP_SECOND)
-    os.system("biliup renew 2>&1 > /dev/null")
-    with open("cookies.json", encoding="utf8") as tmp:
-        data = tmp.read()
-        update_gist(gist_id, token, COOKIE_FILE, json.loads(data))
-    os.remove("cookies.json")
-
+    try:
+        config, cookie, uploaded = get_gist(gist_id, token)
+        with open("cookies.json", "w", encoding="utf8") as tmp:
+            tmp.write(json.dumps(cookie))
+        need_to_process = get_all_video(config)
+        need = select_not_uploaded(need_to_process, uploaded)
+        for i in need:
+            ret = process_one(i["detail"], i["config"])
+            if ret is None:
+                continue
+            i["ret"] = ret
+            uploaded[i["detail"]["vid"]] = i
+            update_gist(gist_id, token, UPLOADED_VIDEO_FILE, uploaded)
+            logging.info(
+                f'上传完成,vid:{i["detail"]["vid"]},aid:{ret["data"]["aid"]},bvid:{ret["data"]["bvid"]}')
+            logging.debug(f"防验证码，暂停 {UPLOAD_SLEEP_SECOND} 秒")
+            time.sleep(UPLOAD_SLEEP_SECOND)
+        os.system("biliup renew 2>&1 > /dev/null")
+        with open("cookies.json", encoding="utf8") as tmp:
+            data = tmp.read()
+            update_gist(gist_id, token, COOKIE_FILE, json.loads(data))
+        os.remove("cookies.json")
+    except Exception as e:
+        logging.error(f"上传处理过程中出错: {e}")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
